5954d182b874580d40f0c8c223318b667bedafc2
guilhermefill <guilhermefill@gmail.com>
2021-11-27 15:08:41 +0100

    done

--- calculator.js
+++ calculator.js
@@ -1,13 +1,33 @@
 function sum(a, b) {
-  return;
+  if (!a && !b) {
+    return 0;
+  } else if (!a) {
+    return b + 0;
+  } else if (!b) {
+    return a +0;
+  } else {
+    return a + b;
+  }
 }
 
 function subtract(a, b) {
-  return;
+  if (!a && !b) {
+    return 0;
+  } else if (!b) {
+    return a - 0;
+  } else if (!a) {
+    return b - 0;
+  } else {
+    return a-b;
+  }
 }
 
 function divide(a, b) {
-  return;
+  if (a === 0 || b === 0) {
+    throw new Error('You can not divide by 0');
+  } else {
+    return a/b;
+  }
 }
 
 function multiply(a, b) {
